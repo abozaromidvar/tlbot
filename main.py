3fcd8176ae5f4e734715965152ca0bec29d69478
abozaromidvar <43962787+abozaromidvar@users.noreply.github.com>
2025-12-05 22:38:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Update
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -12,34 +13,43 @@ router = Router()
 dp.include_router(router)
 
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = "https://wide-areas-marry.loca.lt" + WEBHOOK_PATH
+
+# ❗❗ آدرس وبهوک باید آدرس رسمی Render باشد:
+# https://YOUR-SERVICE.onrender.com/webhook
+RENDER_URL = os.getenv("RENDER_EXTERNAL_URL")
+WEBHOOK_URL = RENDER_URL + WEBHOOK_PATH
 
 
-# هندلر
 @router.message()
 async def echo(message):
     await message.reply("سلام ابوذر! ربات webhook فعاله.")
 
-# aiohttp app
+
 app = web.Application()
 
-# مسیر دریافت آپدیت از تلگرام
+
 async def handle(request):
     data = await request.json()
     update = Update.model_validate(data)
     await dp.feed_update(bot, update)
     return web.Response()
 
+
 app.router.add_post(WEBHOOK_PATH, handle)
+
 
 async def on_startup(app):
     await bot.set_webhook(WEBHOOK_URL)
 
+
 async def on_shutdown(app):
     await bot.delete_webhook()
+
 
 app.on_startup.append(on_startup)
 app.on_shutdown.append(on_shutdown)
 
+
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    web.run_app(app, host="0.0.0.0", port=port)
